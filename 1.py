2f40099e7800ba7d905af8cc3c879b72f7f4b351
lightalexey <tihonov.a@bk.ru>
2022-01-27 11:15:33 +0300

    1

--- 1.py
+++ 1.py
@@ -2,4 +2,16 @@ print('Моя первая программа')
 print('Как тебя зовут?')
 name = input()
 print('Рад познакомиться,', name, '!')
+age = int(input('Сколько тебе лет?'))
+year = 2022 - age
+print('Значит ты родился в ', year, 'году!')
 
+a = int(input('Введи а='))
+b = int(input('Введи b='))
+if a > b:
+    print('a>b')
+else:  # а меньше или равно b
+    if a == b:
+        print('числа равны')
+    else:
+        print('a меньше b')
